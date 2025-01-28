6482d36dd3767a5e912746fcc51d086936fd4a64
Ali Aref <aliaref.dev@gmail.com>
2025-01-28 15:01:19 +0430

    update(color-scheme): set to onedark

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,5 +6,5 @@ require("config.mapping")
 require("config.opts")
 
 -- colorscheme
-vim.cmd.colorscheme("catppuccin-macchiato") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
--- vim.cmd.colorscheme("poimandres")
+-- vim.cmd.colorscheme("catppuccin-macchiato") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+vim.cmd.colorscheme("onedark")
