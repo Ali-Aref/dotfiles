f890ee6f8c173a7e1d995bf28d8b24859a9c31e4
Ali Aref <aliaref.dev@gmail.com>
2025-11-09 10:24:21 +0430

    fix(linking): lsp-file-operations with nvim-tree

--- nvim/lua/plugins/nvim-lsp-file-operations.lua
+++ nvim/lua/plugins/nvim-lsp-file-operations.lua
@@ -4,7 +4,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			-- Uncomment whichever supported plugin(s) you use
-			-- "nvim-tree/nvim-tree.lua",
+			"nvim-tree/nvim-tree.lua",
 			-- "nvim-neo-tree/neo-tree.nvim",
 			-- "simonmclean/triptych.nvim"
 		},
